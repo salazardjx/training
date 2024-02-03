93956bc123acc2f9f5eb6a392047b58f121903f4
salazardjx <87030669+salazardjx@users.noreply.github.com>
2024-02-03 16:39:20 -0300

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,9 @@
 import datetime
 
-	def main():
+def main():
 		print("Hello, world")
-		
+
 		current_time=datetime.datetime.now()
 		print("Tiempo Actual",current_time)
-	if __name__=="__main__":
+if __name__=="__main__":
 		main()
